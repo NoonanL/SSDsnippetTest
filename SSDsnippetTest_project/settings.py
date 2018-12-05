9fd62349415cf701781875fcbc22f66f99b4d445
Liam Noonan <liamnoonan@apples-MacBook-Air.local>
2018-12-05 14:26:44 +0000

    changes to reset database

--- SSDsnippetTest_project/settings.py
+++ SSDsnippetTest_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['lit-bastion-49684.herokuapp.com']
+ALLOWED_HOSTS = ['lit-bastion-49684.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
